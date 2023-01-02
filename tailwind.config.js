b9cc4c847d53d4314cd17fe8dafaf428b8896ad4
momhiar <momhiar@gmail.com>
2023-01-02 21:13:55 +0330

    add: tailwind colors and screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx}",
-  ],
+  content: ["./src/**/*.{js,jsx}"],
   theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    colors: {
+      darkest: "#212121",
+      darker: "#424242",
+      dark: "616161",
+    },
     extend: {},
   },
   plugins: [],
-}
+};
