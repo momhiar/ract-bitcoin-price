08c2344013d50a54d28e2e431640c9aa212f50c2
momhiar <momhiar@gmail.com>
2023-01-02 22:44:37 +0330

    add: chart boxes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import "./styles/globals.scss";
 import MainLayout from "./components/Layouts/mainLayout";
+import styles from "./styles/app.module.scss";
 function App() {
   return (
     <>
-      <MainLayout />
+      <MainLayout>
+        <div className="w-full flex-wrap items-start lg:flex lg:justify-between max-w-[1400px]">
+          <div className={`${styles.box} w-full lg:w-[60%]`}>test</div>
+          <div className={`${styles.box} w-full lg:w-[35%]`}>test</div>
+          <div className={`${styles.box} w-full`}>test</div>
+        </div>
+      </MainLayout>
     </>
   );
 }
-
 export default App;
